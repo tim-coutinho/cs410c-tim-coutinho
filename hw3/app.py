b62113b26a26f894437ca98c7d417bd4b064d451
Tim Coutinho <tmcoutinho42@gmail.com>
2018-10-24 00:11:50 -0700

    Copy hw2 to hw3

---
+++ hw3/app.py
@@ -0,0 +1,25 @@
+"""
+A simple guestbook flask app.
+"""
+import flask
+from flask.views import MethodView
+from index import Index
+from add_recipe import AddRecipe
+from recipes import Recipes
+
+app = flask.Flask(__name__)  # Our Flask app
+
+app.add_url_rule('/',
+                 view_func=Index.as_view('index'),
+                 methods=["GET"])
+
+app.add_url_rule('/add_recipe/',
+                 view_func=AddRecipe.as_view('add_recipe'),
+                 methods=['GET', 'POST'])
+
+app.add_url_rule('/recipes/',
+                 view_func=Recipes.as_view('recipes'),
+                 methods=['GET', 'POST'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ hw3/recipes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from flask.views import MethodView
+import gbmodel
+
+
+class Recipes(MethodView):
+    def get(self):
+        model = gbmodel.get_model()
+        recipes = [
+            dict(title=row[0], author=row[1], ingredients=row[2],
+                 time=row[3], skill=row[4], description=row[5])
+            for row in model.select()
+        ]
+        return render_template('recipes.html', recipes=recipes)
