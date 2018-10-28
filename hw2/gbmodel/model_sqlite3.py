faf324f3425788aba8ab41ae7824f053f82833b6
Tim Coutinho <tmcoutinho42@gmail.com>
2018-10-27 22:46:20 -0700

    Database implementation

--- hw2/gbmodel/model_sqlite3.py
+++ hw2/gbmodel/model_sqlite3.py
@@ -1,68 +1,69 @@
 """
-A simple guestbook flask app.
-ata is stored in a SQLite database that looks something like the following:
-
-+------------+------------------+------------+----------------+
-| Name       | Email            | signed_on  | message        |
-+============+==================+============+----------------+
-| John Doe   | jdoe@example.com | 2012-05-28 | Hello world    |
-+------------+------------------+------------+----------------+
-
-This can be created with the following SQL (see bottom of this file):
-
-    create table guestbook (name text, email text, signed_on date, message);
-
+A simple flask app to keep track of recipes.
+Data is stored in a SQLite database with the columns title, author,
+ingredients, time, skill, and description.
 """
 from datetime import date
 from .Model import Model
 import sqlite3
-DB_FILE = 'entries.db'  # File for our database
+DB_FILE = 'entries.db'  # Local file for our database
 
 
 class model(Model):
     def __init__(self):
-        # Make sure our database exists
+        super().__init__()  # Set defaults
         connection = sqlite3.connect(DB_FILE)
         cursor = connection.cursor()
         try:
-            cursor.execute('SELECT COUNT(rowid) FROM guestbook')
+            cursor.execute('SELECT COUNT(title) FROM recipes')
         except sqlite3.OperationalError:
             cursor.execute('''
-                CREATE TABLE guestbook
-                (name text, email text, signed_on date, message)''')
+                CREATE TABLE recipes
+                (title TEXT, author TEXT, ingredients TEXT, time INTEGER, skill INTEGER, description TEXT)''')
+            for recipe in self.defaults:  # Insert defaults after creating table
+                self.insert(
+                    recipe['title'], recipe['author'],
+                    recipe['ingredients'], recipe['time'],
+                    recipe['skill'], recipe['description'])
+        # Below doesn't work if we want to only insert the defaults if the table is being created
+        # cursor.execute('''
+        #     CREATE TABLE IF NOT EXISTS recipes
+        #     (title TEXT, author TEXT, ingredients TEXT, time INTEGER, skill INTEGER, description TEXT)''')
         cursor.close()
 
     def select(self):
         """
-        Gets all rows from the database
-        Each row contains: name, email, date, message
-        :return: List of lists containing all rows of database
+        Return all recipes in the database. Each list in recipes contains a
+        title, author, ingredient list, time, skill, and description.
+        :return: List of lists
         """
         connection = sqlite3.connect(DB_FILE)
         cursor = connection.cursor()
-        cursor.execute('SELECT * FROM guestbook')
+        cursor.execute('SELECT * FROM recipes')
         return cursor.fetchall()
 
-    def insert(self, name, email, message):
+    def insert(self, title, author, ingredients, time, skill, description):
         """
-        Inserts entry into database
-        :param name: String
-        :param email: String
-        :param message: String
+        Insert a recipe entry into the database.
+        :param title: String
+        :param author: String
+        :param ingredients: String
+        :param time: Integer
+        :param skill: Integer
+        :param description: String
         :return: True
         :raises: Database errors on connection and insertion
         """
-        params = {
-            'name':name,
-            'email':email,
-            'date':date.today(),
-            'message':message
-        }
+        params = dict(zip(
+            ['Title', 'Author', 'Ingredients', 'Time', 'Skill', 'Description'],
+            [title, author, ingredients, time, skill, description]))
         connection = sqlite3.connect(DB_FILE)
         cursor = connection.cursor()
         cursor.execute('''
-            INSERT INTO guestbook (name, email, signed_on, message)
-            VALUES (:name, :email, :date, :message)''', params)
+            INSERT INTO recipes
+            (title, author, ingredients, time, skill, description)
+            VALUES (:Title, :Author, :Ingredients, :Time, :Skill, :Description)
+            ''', params)
 
         connection.commit()
         cursor.close()
