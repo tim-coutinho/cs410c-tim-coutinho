fd1277289a60c7b33dff1b2365437efff2409ae9
Tim Coutinho <tmcoutinho42@gmail.com>
2018-11-29 22:25:39 -0800

    Add nutrition information for each recipe

--- final/add_recipe.py
+++ final/add_recipe.py
@@ -1,5 +1,8 @@
+import json
+import requests
 from flask import redirect, request, url_for, render_template
 from flask.views import MethodView
+
 import gbmodel
 
 
@@ -14,7 +17,31 @@ class AddRecipe(MethodView):
         """
         model = gbmodel.get_model()
         args = request.form
+        ingredients = args['ingredients'].split('\r\n')
+        url = 'https://trackapi.nutritionix.com/v2/natural/nutrients'
+        query = json.dumps({'query': ', '.join(ingredients)})
+        headers = {
+            'x-app-key': '4a900d29f6416233ec26668100c24dc2',
+            'Content-Type': 'application/json',
+            'x-app-id': '0f3a292c'
+        }
+        res = requests.post(url, headers=headers, data=query)
+        if res.ok:
+            tooltip = self.get_nutrition(json.loads(res.text)['foods'])
+        else:
+            tooltip = 'Error acquiring nutritional information.'
+
         model.insert(
-            args['title'], args['author'], args['ingredients'].split('\r\n'),
-            int(args['time']), int(args['skill']), args['description'])
+            args['title'], args['author'], ingredients, int(args['time']),
+            int(args['skill']), args['description'], tooltip)
         return redirect(url_for('index'))
+
+    def get_nutrition(self, foods):
+        calories = protein = carbs = fat = 0
+        for food in foods:
+            calories += food['nf_calories']
+            protein += food['nf_protein']
+            carbs += food['nf_total_carbohydrate']
+            fat += food['nf_total_fat']
+
+        return 'Calories: {}\nProtein: {}g\nCarbs: {}g\nFat: {}g'.format(round(calories), round(protein), round(carbs), round(fat))
--- final/recipes.py
+++ final/recipes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask.views import MethodView
+
 import gbmodel
 
 
@@ -7,8 +8,8 @@ class Recipes(MethodView):
     def get(self):
         model = gbmodel.get_model()
         recipes = [
-            dict(title=row[0], author=row[1], ingredients=row[2],
-                 time=row[3], skill=row[4], description=row[5])
+            dict(title=row[0], author=row[1], ingredients=row[2].split('\n'),
+                 time=row[3], skill=row[4], description=row[5], tooltip=row[6])
             for row in model.select()
         ]
         return render_template('recipes.html', recipes=recipes)
